be7013e1cd6bc8ac619fadd895de17ecbadfabc2
mfalmegriffin <mfalmegriffin@bposeats.com>
2023-04-26 18:57:05 +0300

    initial wagtail folder

---
+++ mfalme/mfalme/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-q4i!7r-82)^!kip(=!g&3mx4%tob1dzp7+vh7hc_ilxic9uce5"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
